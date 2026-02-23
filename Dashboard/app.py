c5e2f44399be1893d67df761439d777eb78b6cf4
Altamash-Abbasi <altamashabbasi001@gmail.com>
2026-02-23 15:29:09 +0530

    minor update

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -80,10 +80,10 @@ file = st.file_uploader("Upload a CSV file", type="csv")
 st.write("The uploaded csv file must have columns \norder date,region,category,sales,profit,discount")
 if file is not None:
     df = load_clean(file)   # pass uploaded file
-    interface(df)
+#    interface(df)
 else:
     df = load_clean(csv_path)  # default file
-    interface(df)
+#    interface(df)
 
 # ------------------ SIDEBAR FILTERS ------------------
 
