d5f202e708605af4e4ef1378b859eefad8c5537a
Altamash-Abbasi <altamashabbasi001@gmail.com>
2026-02-23 14:37:38 +0530

    Adding file uploading functionality

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -21,55 +21,65 @@ from Scripts.insights import insights
 # ------------------ LOAD DATA ------------------
 df = load_clean(csv_path)  # ✅ Use the absolute path
 
-# ------------------ KPIs ------------------
-total_revenue, total_profit, profit_margin = compute_kpis(df)
+def interface():
+    # ------------------ KPIs ------------------
+    total_revenue, total_profit, profit_margin = compute_kpis(df)
 
-st.title("ABIDS — Automated Business Intelligence Dashboard")
+    st.title("ABIDS — Automated Business Intelligence Dashboard")
 
-st.header("Executive KPIs")
-st.metric("Total Revenue", f"{total_revenue:,.0f}")
-st.metric("Total Profit", f"{total_profit:,.0f}")
-st.metric("Profit Margin", f"{profit_margin:.2f}%")
+    st.header("Executive KPIs")
+    st.metric("Total Revenue", f"{total_revenue:,.0f}")
+    st.metric("Total Profit", f"{total_profit:,.0f}")
+    st.metric("Profit Margin", f"{profit_margin:.2f}%")
 
-# ------------------ LEAKAGE ------------------
-product_analysis, high_risk, watchlist,top = detect_profit_leakage(df)
+    # ------------------ LEAKAGE ------------------
+    product_analysis, high_risk, watchlist,top = detect_profit_leakage(df)
 
-st.header("High Risk Products")
-st.dataframe(high_risk.head(10))
+    st.header("High Risk Products")
+    st.dataframe(high_risk.head(10))
 
-st.header("Watchlist Products")
-st.dataframe(watchlist.head(10))
+    st.header("Watchlist Products")
+    st.dataframe(watchlist.head(10))
 
-st.header("Top Loss Making Products")
-st.dataframe(top)
+    st.header("Top Loss Making Products")
+    st.dataframe(top)
 
-# ------------------ RECOMMENDATIONS ------------------
-recommendations = generate_recommendation(product_analysis)
+    # ------------------ RECOMMENDATIONS ------------------
+    recommendations = generate_recommendation(product_analysis)
 
-st.header("Recommendations")
+    st.header("Recommendations")
 
-for r in recommendations[:10]:
-    st.write(f"Product: {r[0]} | Category: {r[1]} | Action: {r[2]}")
+    for r in recommendations[:10]:
+        st.write(f"Product: {r[0]} | Category: {r[1]} | Action: {r[2]}")
 
-# -------------------- INSIGHTS ---------------------
-top_customer, profit_region, loss_region, category, loss_per, leak_regions = insights(df)
+    # -------------------- INSIGHTS ---------------------
+    top_customer, profit_region, loss_region, category, loss_per, leak_regions = insights(df)
 
-st.header("Business Insights")
+    st.header("Business Insights")
 
-st.subheader("Top Customers by Profit")
-st.dataframe(top_customer)
+    st.subheader("Top Customers by Profit")
+    st.dataframe(top_customer)
 
-st.subheader("Most Profitable Regions")
-st.dataframe(profit_region)
+    st.subheader("Most Profitable Regions")
+    st.dataframe(profit_region)
 
-st.subheader("Loss Making Regions")
-st.dataframe(loss_region)
+    st.subheader("Loss Making Regions")
+    st.dataframe(loss_region)
 
-st.subheader("Category Analysis")
-st.dataframe(category)
+    st.subheader("Category Analysis")
+    st.dataframe(category)
 
-st.subheader("Worst Performing Category Loss %")
-st.write(loss_per)
+    st.subheader("Worst Performing Category Loss %")
+    st.write(loss_per)
 
-st.subheader("High Sales but Low Profit Region (Leakage)")
-st.dataframe(leak_regions)
+    st.subheader("High Sales but Low Profit Region (Leakage)")
+    st.dataframe(leak_regions)
+
+    
+file = st.file_uploader("Upload a CSV file", type="csv")
+if file is not None:
+    df = load_clean(file)   # pass uploaded file
+    interface()
+else:
+    df = load_clean(csv_path)  # default file
+    interface()
